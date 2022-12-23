498bcbbdbfa3bc51aadbce6f84d44d94cf964275
Braden Donaldson <bsdonald7@gmail.com>
2022-12-23 08:26:10 -0700

    added some buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Hello World!'),
         ),
-        body: const Text('This is [not] my first app!'),
+        body: Center(
+          child: Column(
+            children: [
+              const Text('This is [not] my first app!'),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Hey look, a button!'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Oh no! Another button!'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Whatever will you do?'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
