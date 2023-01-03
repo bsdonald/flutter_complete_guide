4082bd5744873dbfc0d7dc6966bd589348848260
Braden Donaldson <bsdonald7@gmail.com>
2023-01-03 13:17:03 -0700

    added const to myApp.dart

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/home_screen.dart';
+import 'package:shop_app/home/home_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return const MaterialApp(home: HomeScreen());
   }
 }
