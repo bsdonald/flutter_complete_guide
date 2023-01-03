a68b1080f4deb15a61e8b9dcd1bcb2787501a081
Braden Donaldson <bsdonald7@gmail.com>
2023-01-03 13:16:30 -0700

    added contants file added home folder

---
+++ lib/home/constants.dart
@@ -0,0 +1,14 @@
+class HomeConstants {
+  final String title = 'Hello World';
+
+  //prompts
+  final String promptOne = 'This is [not] my first app!';
+  final String promptTwo = 'Hey, that tickles!';
+  final String promptThree = 'Stop that!';
+  final String promptFour = 'I\'m telling mom!!!!';
+
+  //buttons
+  final String buttonOne = 'Hey look, a button!';
+  final String buttonTwo = 'Oh no! Another button!';
+  final String buttonThree = 'Whatever will you do?';
+}
---
+++ lib/home/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/home/constants.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _promptIndex = 0;
+
+  void _buttonPress() {
+    setState(() {
+      _promptIndex = 1;
+    });
+    print(_promptIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeConstants constants = HomeConstants();
+    List<String> prompts = [
+      constants.promptOne,
+      constants.promptTwo,
+      constants.promptThree,
+      constants.promptFour,
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(constants.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(prompts[_promptIndex]),
+            ElevatedButton(
+              child: Text(constants.buttonOne),
+              onPressed: () => _buttonPress(),
+            ),
+            ElevatedButton(
+              child: Text(constants.buttonTwo),
+              onPressed: () => _buttonPress(),
+            ),
+            ElevatedButton(
+              child: Text(constants.buttonThree),
+              onPressed: () => _buttonPress(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
