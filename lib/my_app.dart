4a794834626b386679cec023cf558dbae724f966
Braden Donaldson <bsdonald7@gmail.com>
2023-10-20 12:44:11 -0700

    added Roll Dice Home Screen

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/home/home_screen.dart';
 import 'package:shop_app/quiz/quiz_home_screen.dart';
+import 'package:shop_app/roll_dice/roll_dice_home_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const HomeScreen(),
         '/quiz': (context) => const QuizHomeScreen(),
+        '/dice': (context) => const RollDiceHomeScreen(),
       },
     );
   }
